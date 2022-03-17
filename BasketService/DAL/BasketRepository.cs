0437d61a0a1fa2d1df8c6c945b55ea2cb3ce530d
Tibi3k <Solyomtibi2000@gmail.com>
2022-03-17 23:07:30 +0100

    added BasketRepository defaults

--- BasketService/DAL/BasketRepository.cs
+++ BasketService/DAL/BasketRepository.cs
@@ -1,6 +1,27 @@
-﻿namespace BasketService.DAL
+﻿using BasketService.Model;
+
+namespace BasketService.DAL
 {
     public class BasketRepository : IBasketRepository
     {
+        public void AddProductToBasket(Product product)
+        {
+            throw new NotImplementedException();
+        }
+
+        public UserBasket FindBasketById(string id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public UserBasket FindBasketByUserId(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<UserBasket> getAllBaskets()
+        {
+            throw new NotImplementedException();
+        }
     }
 }
