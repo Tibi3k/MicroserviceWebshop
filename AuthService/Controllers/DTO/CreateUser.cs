1b69077991c70c56c8da15dee6ed524ccef32c57
Tibi3k <Solyomtibi2000@gmail.com>
2022-05-17 21:41:26 +0200

    removed auth service

--- AuthService/Controllers/DTO/CreateUser.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace AuthService.Controllers.DTO;
-
-public class CreateUser
-{
-    public string Username { get; set; }
-    public string EmailAddress { get; set; }
-    public string Password { get; set; }
-}
--- AuthService/Controllers/DTO/LoginUser.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace AuthService.Controllers.DTO;
-
-public class LoginUser
-{
-    public string Username { get; set; }
-    public string Password { get; set; }
-}
