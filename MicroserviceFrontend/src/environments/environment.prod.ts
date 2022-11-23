746d79f68dac1c5797812fac8ee492300c895086
solyomtibor <Solyomtibi2000@gmail.com>
2022-11-23 22:28:51 +0100

    environment update

--- MicroserviceFrontend/src/environments/environment.prod.ts
+++ MicroserviceFrontend/src/environments/environment.prod.ts
@@ -1,9 +1,9 @@
 export const environment = {
   production: true,
-  apiUrl: 'http://localhost:80/',
-  redirectUrl: 'http://localhost',
+  apiUrl: 'https://20.238.248.85/',
+  redirectUrl: 'https://20.238.248.85',
   authority: 'https://microwebshop.b2clogin.com/microwebshop.onmicrosoft.com/B2C_1_singinorsingup',
   scopeUrl: 'https://microwebshop.onmicrosoft.com/1716b024-dc4d-477f-a566-680069a38887/API.Access',
   clientId: 'b31a2e64-68eb-424a-848d-d0c9b65bf0a0',
-  protectedRoute: 'http://localhost',
+  protectedRoute: 'https://20.238.248.85',
 };
