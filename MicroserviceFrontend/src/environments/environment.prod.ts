9d7dcdce1fda19a861343102b3ac93eb65594f33
solyomtibor <Solyomtibi2000@gmail.com>
2022-11-24 13:56:25 +0100

    ip update

--- MicroserviceFrontend/src/environments/environment.prod.ts
+++ MicroserviceFrontend/src/environments/environment.prod.ts
@@ -1,9 +1,9 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://20.4.177.105/',
-  redirectUrl: 'https://20.4.177.105',
+  apiUrl: 'https://20.23.207.99/',
+  redirectUrl: 'https://20.23.207.99',
   authority: 'https://microwebshop.b2clogin.com/microwebshop.onmicrosoft.com/B2C_1_singinorsingup',
   scopeUrl: 'https://microwebshop.onmicrosoft.com/1716b024-dc4d-477f-a566-680069a38887/API.Access',
   clientId: 'b31a2e64-68eb-424a-848d-d0c9b65bf0a0',
-  protectedRoute: 'https://20.4.177.105',
+  protectedRoute: 'https://20.23.207.99',
 };
